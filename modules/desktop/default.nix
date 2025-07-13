1bd62aee5e5a46321d00928e919c2b7543961dcd
Jon Thor Kristinsson <git@jontk.com>
2025-07-13 23:52:20 +0100

    fix: remove QT_QPA_PLATFORM from desktop module
    Remove duplicate QT_QPA_PLATFORM setting as it's already defined
    in the hyprland module with a more flexible value that supports
    both wayland and xcb backends.

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -148,7 +148,6 @@
       # Wayland-specific
       NIXOS_OZONE_WL = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       SDL_VIDEODRIVER = "wayland";
       CLUTTER_BACKEND = "wayland";
