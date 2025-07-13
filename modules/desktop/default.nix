48899b8108477080353d1f5841860f9c4a775cd2
Jon Thor Kristinsson <git@jontk.com>
2025-07-13 22:08:51 +0100

    fix: remove duplicate xdg-desktop-portal-hyprland
    The xdg-desktop-portal-hyprland service is automatically enabled when
    programs.hyprland.enable is set to true. Remove the manual addition
    from extraPortals to avoid the symbolic link conflict.

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -216,7 +216,7 @@
       wlr.enable = true;
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
+        # xdg-desktop-portal-hyprland is automatically enabled by programs.hyprland
       ];
       config = {
         common = {
