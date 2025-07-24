3d066fc4eb79674dad151e9fec5da5c7e6a0ae20
Jon Thor Kristinsson <git@jontk.com>
2025-07-24 19:35:06 +0000

    feat: add editors module integration placeholder
    - Created editors module structure
    
    - Placeholder for vim/neovim configuration
    
    Related to Task 2.4 in dotfiles integration PRD

---
+++ modules/dotfiles-integration/modules/editors.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  # This module will be populated when we properly implement the home-manager integration
+  # For now, it's a placeholder to avoid evaluation errors
+}
